45f3ac151fc21d459e2515c3aff97cd4bf877bf8
Alexandre Alapetite <alexandre.alapetite@alexandra.dk>
2024-07-10 15:36:01 +0200

    Fix for PHP 8.4 (#227)
    > Deprecated: Gt\CssXPath\Translator::preg_match_collated(): Implicitly marking parameter $transform as nullable is deprecated, the explicit nullable type must be used instead in ./phpgt/cssxpath/src/Translator.php on line 279

--- src/Translator.php
+++ src/Translator.php
@@ -340,7 +340,7 @@ class Translator {
 	protected function preg_match_collated(
 		string $regex,
 		string $string,
-		callable $transform = null
+		?callable $transform = null
 	):array {
 		preg_match_all(
 			$regex,
